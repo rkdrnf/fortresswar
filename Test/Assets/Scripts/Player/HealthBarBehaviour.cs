bdbad1fd1be5b6235907ccd0f232c3e7c87bd033
rkdrnf <rkdrnf@gmail.com>
2014-12-12 20:04:44 +0900

    ??? ??

--- Test/Assets/Scripts/Player/HealthBarBehaviour.cs
+++ Test/Assets/Scripts/Player/HealthBarBehaviour.cs
@@ -5,11 +5,13 @@ public class HealthBarBehaviour : MonoBehaviour {
 
     PlayerBehaviour player;
 
+    public Texture2D texture; 
+
     Color fullColor;
     Color dyingColor;
 	// Use this for initialization
 	void Start () {
-	
+        player = transform.parent.gameObject.GetComponent<PlayerBehaviour>();
 	}
 	
 	// Update is called once per frame
@@ -22,6 +24,6 @@ public class HealthBarBehaviour : MonoBehaviour {
         Vector2 targetPos;
         targetPos = Camera.main.WorldToScreenPoint(transform.position);
 
-        GUI.Box(new Rect(targetPos.x, Screen.height - targetPos.y, 60, 20), "Health");
+        GUI.DrawTexture(new Rect(targetPos.x - 20, Screen.height - targetPos.y - 10, player.health / 100 * 40, 5), texture, ScaleMode.StretchToFill, false); //displays a healthbar
     }
 }
