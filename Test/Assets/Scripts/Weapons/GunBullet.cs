06069fcb13f8d40a05a065a9ec02031d879d68da
milkywing01 <milkywing01@gmail.com>
2014-12-15 19:02:19 +0900

    ??? ??

--- Test/Assets/Scripts/Weapons/GunBullet.cs
+++ Test/Assets/Scripts/Weapons/GunBullet.cs
@@ -3,90 +3,40 @@ using System.Collections;
 
 public class GunBullet : Projectile {
 
-
-
-	const int DAMAGE = 20;
-
-	const int RANGE = 30;
-
-	Vector3 startPosition;
-	Vector3 currentPosition;
-
-    public GameObject explosionAnimation;
-
-    bool isQuitting = false;
-
-	// Use this for initialization
-	void Awake () {
-        
-		startPosition = transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// Draw in Client, Collide in Server
-		//Client
-
-		currentPosition = transform.position;
-
-		Quaternion rot = Quaternion.FromToRotation (Vector3.right, new Vector3 (rigidbody2D.velocity.x, rigidbody2D.velocity.y));
-		transform.rotation = rot;
-
-        if ((currentPosition - startPosition).sqrMagnitude > RANGE * RANGE)
-        {
-            Destroy();
-        }
-	}
-
-	void OnTriggerEnter2D(Collider2D targetCollider)
+	protected override void CollisionFunc(Collider2D targetCollider)
 	{
         Debug.Log("collided");
 		if (Network.isServer) {
-			if (targetCollider.gameObject.CompareTag ("Tile")) {
-				OnCollideToTile(targetCollider);
-			} else if (targetCollider.gameObject.CompareTag("Player")){
+            if (targetCollider.gameObject.CompareTag("Tile"))
+            {
+                OnCollideToTile(targetCollider);
+            }
+            else if (targetCollider.gameObject.CompareTag("Player"))
+            {
                 OnCollideToPlayer(targetCollider);
 			}
 		}
         return;
-
 	}
 
     void OnCollideToTile(Collider2D targetCollider)
     {
-        GameObject tile = targetCollider.gameObject;
-        tile.GetComponent<Tile>().Damage(DAMAGE);
-
-        Destroy();
-
-        
+        Tile tile = targetCollider.gameObject.GetComponent<Tile>();
+        if (tile)
+        {
+            tile.GetComponent<Tile>().Damage(damage);
+        }
     }
 
     void OnCollideToPlayer(Collider2D targetCollider)
     {
         //When Hit My Player
         PlayerBehaviour character = targetCollider.gameObject.GetComponent<PlayerBehaviour>();
-        if (owner == character.GetOwner()) 
-            return;
-
-
-
-        character.Damage(DAMAGE, new NetworkMessageInfo());
-
-        Destroy();
-    }
-
-    void OnApplicationQuit()
-    {
-        isQuitting = true;
-    }
-
-    void OnDestroy()
-    {
-        if (isQuitting)
-            return;
-
-        GameObject explosion = (GameObject)Instantiate(explosionAnimation, transform.position, transform.rotation);
-        Destroy(explosion, 0.4f);
+        if (character)
+        {
+            if (owner == character.GetOwner())
+                return;
+            character.Damage(damage, new NetworkMessageInfo());
+        }
     }
 }
--- Test/Assets/Scripts/Weapons/Projectile.cs
+++ Test/Assets/Scripts/Weapons/Projectile.cs
@@ -7,22 +7,59 @@ using UnityEngine;
 public abstract class Projectile : MonoBehaviour
 {
     public long ID;
-    
     public NetworkPlayer owner;
+
+    public int damage;
+    public int range;
+
+    protected Vector3 startPosition;
+    public GameObject explosionAnimation;
 
     void Awake()
     {
+        startPosition = transform.position;
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        // Draw in Client, Collide in Server
+        //Client
 
+        Quaternion rot = Quaternion.FromToRotation(Vector3.right, new Vector3(rigidbody2D.velocity.x, rigidbody2D.velocity.y));
+        transform.rotation = rot;
 
-    public void Destroy()
+        if ((transform.position - startPosition).sqrMagnitude > range * range)
+        {
+            Destroy();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D targetCollider)
+    {
+        CollisionFunc(targetCollider);
+        Destroy();
+    }
+
+    protected abstract void CollisionFunc(Collider2D targetCollider);
+
+    void OnApplicationQuit()
+    {
+        DestroyImmediate(this);
+    }
+
+    private void Destroy()
     {
         if (Network.isServer)
         {
             Packet.S2C.DestroyProjectile pck = new Packet.S2C.DestroyProjectile(ID);
             Game.Instance.DestroyProjectile(pck.Serialize());
         }
+    }
 
+    void OnDestroy()
+    {
+        GameObject explosion = (GameObject)Instantiate(explosionAnimation, transform.position, transform.rotation);
+        Destroy(explosion, 0.4f);
     }
 }
