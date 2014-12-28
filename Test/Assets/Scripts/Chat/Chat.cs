d4c43c21610d8bbddcaab3afa12fc8b1ffa886f7
rkdrnf <rkdrnf@gmail.com>
2014-12-29 00:38:42 +0900

    ?? ?? ?? ??, ?? ?? ??? ?? ??.

--- Test/Assets/Scripts/Chat/Chat.cs
+++ Test/Assets/Scripts/Chat/Chat.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
+using Client;
+
 public class Chat
 {
 	public Chat(int playerID, string chat)
     {
         this.playerID = playerID;
         this.text = chat;
+
+        if (C_PlayerManager.Inst.GetSetting(playerID) != null)
+            this.writer = C_PlayerManager.Inst.GetSetting(playerID).name;
     }
     public string text;
+    public string writer;
     public int playerID;
 }
