3d5aed53ca7ca86dc8b0202e395b75665c73cc10
milkywing01 <milkywing01@gmail.com>
2014-12-29 19:38:48 +0900

    NGUI ??

---
+++ Test/Assets/UI/Scripts/Panel_Main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Panel_Main : MonoBehaviour {
+
+    public Menu menu;
+    public UIButton Btn_StartServer;
+    public UIButton Btn_RefreshServer;
+
+    public void BtnFunc_StartServer()
+    {
+        Debug.Log("Click");
+        menu.StartServer();
+        gameObject.SetActive(false);
+    }
+
+    public void BtnFunc_RefreshServer()
+    {
+        menu.RefreshServerList();
+    }
+}
